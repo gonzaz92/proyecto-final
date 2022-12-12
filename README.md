399d93388a95660b2508e2f2a2a054d4b1afafce
Gonza.z92 <gonza.zorro92@gmail.com>
2022-12-12 17:28:11 -0300

    Ultima actualización 12/12/22

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # proyecto-final
 Mi proyecto final
+
+Buenas, se puede probar en cualquie orden. Lo ideal seguro siempre es cargar un dato nuevo para despues ver si te deja actualizar o borrar.
--- seed_data.py
+++ seed_data.py
@@ -5,8 +5,4 @@ Familiar(nombre="Alberto", direccion="Rio Parana 745", numero_pasaporte=890890).
 Familiar(nombre="Samuel", direccion="Rio Parana 745", numero_pasaporte=345345).save()
 Familiar(nombre="Florencia", direccion="Rio Parana 745", numero_pasaporte=567567).save()
 
-Juegos(nombre='Megaman', tipo= 'Plataformas')
-Juegos(nombre='Zelda, Ocarina Of time', tipo= 'Aventura')
-Juegos(nombre='Golden Sun', tipo= 'Rol')
-
 print("Se cargo con éxito los usuarios de pruebas")
